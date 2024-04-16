c35870cb23a5a6168361b71a4b71005a90d6ab12
SeongKookKIM <ksk8646@naver.com>
2024-04-16 10:36:20 +0900

    Property Wrapper with multiple type

--- MyPlayground13.playground/Contents.swift
+++ MyPlayground13.playground/Contents.swift
@@ -37,3 +37,47 @@ print("\(contact.name), \(contact.city), \(contact.country)")
       var address = Address()
       address.city = "London"
       print(address.city)
+
+
+/*
+ Comparable은 프로토콜(protocol) 중 하나로, 비교 연산자를 지원하는 타입을 정의하기 위해 사용됩니다.
+ */
+@propertyWrapper
+struct MinMaxVal<V: Comparable> {
+    var value: V
+    let min: V
+    let max: V
+    
+    init(wrappedValue: V, min: V, max: V) {
+        value = wrappedValue
+        self.min = min
+        self.max = max
+    }
+    
+    var wrappedValue: V {
+        get { return value }
+        set {
+            if newValue > max {
+                value = max
+            } else if newValue < min {
+                value = min
+            } else {
+                value = newValue
+            }
+        }
+    }
+}
+
+struct Demo {
+    @MinMaxVal (min:10, max:150) var value: Int = 100
+    @MinMaxVal (min:"Apple", max: "Orange") var value2: String = "Banana"
+    @MinMaxVal (min: "2024-01", max: "2024-12") var value3: String = ""
+}
+var demo = Demo()
+
+demo.value2 = "Pear"
+
+print(demo.value2)
+
+demo.value3 = "2025-01"
+print(demo.value3)
