168fb92069efa26ac37f4a93e2b76e8c6dfc7ed9
SeongKookKIM <ksk8646@naver.com>
2024-04-17 09:27:38 +0900

    Error Handle

---
+++ MyPlayground15.playground/Contents.swift
@@ -0,0 +1,41 @@
+
+let connectionOk = true
+let connectionSepeed = 30.00
+let fileFound = false
+
+
+enum FileTransferError: Error {
+    case noConnection
+    case lowBandWidth
+    case fileNotFound
+}
+
+func fileTransfer() throws  {
+    guard connectionOk else {
+        throw FileTransferError.noConnection
+    }
+    
+    guard connectionSepeed > 30 else {
+        throw FileTransferError.lowBandWidth
+    }
+    
+    guard fileFound else {
+        throw FileTransferError.fileNotFound
+    }
+}
+
+
+func sendFile() -> String {
+    do{
+        try fileTransfer()
+    } catch FileTransferError.noConnection {
+        print("No Connection")
+    } catch FileTransferError.lowBandWidth {
+        print("Speed too low")
+    } catch FileTransferError.fileNotFound {
+        print("File Not Found")
+    } catch {
+        return "Unkonwn"
+    }
+    return "Successful transfer"
+}
