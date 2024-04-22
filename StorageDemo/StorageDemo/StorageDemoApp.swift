e7b0870231e7302ce60449a0a46048d79d4944c0
SeongKookKIM <ksk8646@naver.com>
2024-04-22 09:10:53 +0900

    Initial Project StorageDemo

---
+++ StorageDemo/StorageDemo/StorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageDemoApp.swift
+//  StorageDemo
+//
+//  Created by SeongKook on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct StorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
