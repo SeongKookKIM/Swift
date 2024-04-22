aafa87ff55a02b342ea5e63d8ee8f38d2b7d074b
SeongKookKIM <ksk8646@naver.com>
2024-04-22 10:09:44 +0900

    SceneStorage AppStorage

---
+++ StorageDemo/StorageDemo/AppStorageView.swift
@@ -0,0 +1,21 @@
+//
+//  AppStorageView.swift
+//  StorageDemo
+//
+//  Created by SeongKook on 4/22/24.
+//
+
+import SwiftUI
+
+struct AppStorageView: View {
+    @AppStorage("mytext") private var editorText: String = "Sample Text"
+    var body: some View {
+        TextEditor(text: $editorText)
+            .padding(38)
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    AppStorageView()
+}
---
+++ StorageDemo/StorageDemo/SceneStorageView.swift
@@ -0,0 +1,25 @@
+//
+//  SceneStorageView.swift
+//  StorageDemo
+//
+//  Created by SeongKook on 4/22/24.
+//
+
+import SwiftUI
+
+struct SceneStorageView: View {
+    
+    @SceneStorage("mytext") private var editorText: String = ""
+    
+    var body: some View {
+        TextEditor(text: $editorText)
+            .padding(38)
+            .font(.largeTitle)
+    }
+}
+
+
+
+#Preview {
+    SceneStorageView()
+}
